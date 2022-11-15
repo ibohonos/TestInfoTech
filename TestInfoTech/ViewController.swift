8a567d1be14730d3abddf13399ea3fc4903a71b3
ibohonos <The.head1993@gmail.com>
2022-11-15 13:32:29 +0200

    Initial Commit

---
+++ TestInfoTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestInfoTech
+//
+//  Created by Іван Богоносюк on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
